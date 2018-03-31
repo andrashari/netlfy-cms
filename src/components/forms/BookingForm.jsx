2bce26acd12ebd86075976c68a82d145673bbb47
Hplatini89 <hplatini89@gmail.com>
2018-04-01 01:18:16 +0200

    BookinForm added

---
+++ src/components/forms/BookingForm.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+
+import { insertOrder } from '../../mock'
+
+import { Form, DatePicker, Select, Button } from 'antd';
+const Option = Select.Option; 'antd';
+const FormItem = Form.Item;
+const RangePicker = DatePicker.RangePicker;
+
+class BookingForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        const data = JSON.parse(JSON.stringify(values))
+        data.start = new Date(data.interval[0])
+        data.end = new Date(data.interval[1])
+        delete data.interval;
+        console.log(insertOrder(data))
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <FormItem
+          {...formItemLayout}
+          label="Booking dates"
+        >
+          {getFieldDecorator('interval')(
+            <RangePicker />
+          )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Please select Van"
+        >
+          {getFieldDecorator('van')(
+            <Select
+              placeholder="Select a Van"
+            >
+              <Option value="van1">Happy Van</Option>
+              <Option value="van2">Sad Van</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem
+          wrapperCol={{
+            xs: { span: 24, offset: 0 },
+            sm: { span: 16, offset: 8 },
+          }}
+        >
+          <Button type="primary" htmlType="submit">Submit</Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+export default BookingForm = Form.create()(BookingForm);
---
+++ src/mock.js
@@ -0,0 +1,23 @@
+import {
+    StitchClientFactory
+} from 'mongodb-stitch';
+
+const appId = 'alfreskobookingadmin-vwutk';
+const stitchClientPromise = StitchClientFactory.create(appId);
+
+// Orders
+export async function insertOrder(order) {
+    stitchClientPromise.then(stitchClient => {
+        const db = stitchClient.service('mongodb', 'mongodb-atlas').db('Orders');
+        stitchClient
+            .login()
+            .then(() =>
+                db.collection("Orders")
+                .insertOne({
+                    ...{owner_id: stitchClient.authedId()}, ...order
+                })
+            )
+            .then(res => console.log(res))
+            .catch(e => console.log('error: ', e));
+    })
+}
