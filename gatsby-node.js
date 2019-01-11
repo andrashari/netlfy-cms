31279608f6c18844f98019091fece8733b2ae741
Hplatini89 <hplatini89@gmail.com>
2019-01-11 01:26:08 +0100

    gatsby-node reset

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,59 +1,25 @@
-const axios = require('axios');
-const crypto = require('crypto');
-
 const _ = require('lodash')
 const path = require('path')
-const {
-    createFilePath
-} = require('gatsby-source-filesystem')
+const { createFilePath } = require('gatsby-source-filesystem')
+const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
-exports.createPages = ({
-    boundActionCreators,
-    graphql
-}) => {
-    const {
-        createPage
-    } = boundActionCreators
-    // Implement the Gatsby API “onCreatePage”. This is
-    // called after every page is created.
-    exports.onCreatePage = async ({ page }) => {
+exports.createPages = ({ actions, graphql }) => {
+    const { createPage } = actions
 
-        // page.matchPath is a special key that's used for matching pages
-        // only on the client.
-        if (page.path.match(/^\/contact/)) {
-            page.matchPath = "/contact/*"
-
-            // Update the page.
-            createPage(page)
-
-        }
-    }
-    /*
-        teamName
-        teamRole
-        teamImageSource
-        teamIntroduction
-        teamEmail
-        teamPhone
-        testimonialImageSource
-        testimonialName
-        testimonialQuote
-        faqQuestion
-        faqAnswer
-        videoUrl
-        videoPoster
-        instagramUrl
-    */
     return graphql(`
-    query pageQuery {
-      allMarkdownRemark {
+    {
+      allMarkdownRemark(limit: 1000) {
         edges {
-            node {
-                frontmatter {
-                    heroUrl
-                    instagramUrl
-                }
+          node {
+            id
+            fields {
+              slug
             }
+            frontmatter {
+              tags
+              templateKey
+            }
+          }
         }
       }
     }
@@ -62,60 +28,15 @@ exports.createPages = ({
             result.errors.forEach(e => console.error(e.toString()))
             return Promise.reject(result.errors)
         }
-
-        /*const posts = result.data.allMarkdownRemark.edges
-        posts.forEach(edge => {
-          const id = edge.node.id
-          createPage({
-            path: edge.node.fields.slug,
-            tags: edge.node.frontmatter.tags,
-            component: path.resolve(
-              `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-            ),
-            // additional data can be passed via context
-            context: {
-              id,
-            },
-          })
-        })
-        // Tag pages:
-        let tags = []
-        // Iterate through each post, putting all found tags into `tags`
-        posts.forEach(edge => {
-          if (_.get(edge, `node.frontmatter.tags`)) {
-            tags = tags.concat(edge.node.frontmatter.tags)
-          }
-        })
-        // Eliminate duplicate tags
-        tags = _.uniq(tags)
-        // Make tag pages
-        tags.forEach(tag => {
-          const tagPath = `/tags/${_.kebabCase(tag)}/`
-          createPage({
-            path: tagPath,
-            component: path.resolve(`src/templates/tags.js`),
-            context: {
-              tag,
-            },
-          })
-        })*/
     })
 }
 
-exports.onCreateNode = ({
-    node,
-    boundActionCreators,
-    getNode
-}) => {
-    const {
-        createNodeField
-    } = boundActionCreators
+exports.onCreateNode = ({ node, actions, getNode }) => {
+    const { createNodeField } = actions
+    fmImagesToRelative(node) // convert image paths for gatsby images
 
     if (node.internal.type === `MarkdownRemark`) {
-        const value = createFilePath({
-            node,
-            getNode
-        })
+        const value = createFilePath({ node, getNode })
         createNodeField({
             name: `slug`,
             node,
@@ -174,18 +95,3 @@ exports.sourceNodes = async ({ boundActionCreators }) => {
 
     return;
 }
-
-/*exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-    if (stage === "build-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /bad-module/,
-              use: loaders.null(),
-            },
-          ],
-        },
-      })
-    }
-  }*/
